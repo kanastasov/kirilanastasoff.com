c059b9af01fb0d2802c48c41617657b50b3c304d
Kiril Anastasov <kireprogramista@gmail.com>
2020-08-02 08:46:41 +0300

    Expose the client to a service interface rather than the concrete implementation

--- kirilanastasoff.com/src/main/java/com/personal/page/service/PdfService.java
+++ kirilanastasoff.com/src/main/java/com/personal/page/service/PdfService.java
@@ -1,22 +1,20 @@
-//package com.personal.page.service;
-//
-//import java.io.File;
-//import java.io.IOException;
-//
-//import org.thymeleaf.context.Context;
-//
-//import com.lowagie.text.DocumentException;
-//
-//
-//public interface PdfService {
-//
-//	File generatePdf()throws IOException,DocumentException;
-//	
-//	private File renderPdf(String html) throws IOException,DocumentException;
-//	
-//	private Context getContext();
-//	
-//	private String loadAndFillTemplate(Context context);
-//	
-//	
-//}
+package com.personal.page.service;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.thymeleaf.context.Context;
+
+import com.lowagie.text.DocumentException;
+
+public interface PdfService {
+
+	File generatePdf() throws IOException, DocumentException;
+
+	File renderPdf(String html) throws IOException, DocumentException;
+
+	Context getContext();
+
+	String loadAndFillTemplate(Context context);
+
+}
--- kirilanastasoff.com/src/main/java/com/personal/page/service/PdfServiceImp.java
+++ kirilanastasoff.com/src/main/java/com/personal/page/service/PdfServiceImp.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 @Service
-public class PdfServiceImp  {
+public class PdfServiceImp implements PdfService {
 
 	private static final String PDF_RESOURCES = "/pdf-resources/";
 	private UserAccountService userAccountService;
@@ -27,31 +27,31 @@ public class PdfServiceImp  {
 		this.templateEngine = templateEngine;
 	}
 
-    public File generatePdf() throws IOException, DocumentException {
-        Context context = getContext();
-        String html = loadAndFillTemplate(context);
-        return renderPdf(html);
-    }
-
-	private File renderPdf(String html) throws IOException, DocumentException {
-		    File file = File.createTempFile("userAccounts", ".pdf");
-	        OutputStream outputStream = new FileOutputStream(file);
-	        ITextRenderer renderer = new ITextRenderer(20f * 4f / 3f, 20);
-	        renderer.setDocumentFromString(html, new ClassPathResource(PDF_RESOURCES).getURL().toExternalForm());
-	        renderer.layout();
-	        renderer.createPDF(outputStream);
-	        outputStream.close();
-	        file.deleteOnExit();
-	        return file;
+	public File generatePdf() throws IOException, DocumentException {
+		Context context = getContext();
+		String html = loadAndFillTemplate(context);
+		return renderPdf(html);
 	}
 
-	private Context getContext() {
+	public File renderPdf(String html) throws IOException, DocumentException {
+		File file = File.createTempFile("userAccounts", ".pdf");
+		OutputStream outputStream = new FileOutputStream(file);
+		ITextRenderer renderer = new ITextRenderer(20f * 4f / 3f, 20);
+		renderer.setDocumentFromString(html, new ClassPathResource(PDF_RESOURCES).getURL().toExternalForm());
+		renderer.layout();
+		renderer.createPDF(outputStream);
+		outputStream.close();
+		file.deleteOnExit();
+		return file;
+	}
+
+	public Context getContext() {
 		Context context = new Context();
 		context.setVariable("userAccounts", userAccountService.getAllUserAccounts());
 		return context;
 	}
 
-	private String loadAndFillTemplate(Context context) {
+	public String loadAndFillTemplate(Context context) {
 		return templateEngine.process("pdf_userAccounts", context);
 	}
 
